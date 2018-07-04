b648b025b232e06fb71e488da2a332ac24eaa70c
Tomasz Pawlik <k4masz@gmail.com>
2018-07-04 14:33:05 +0200

    Added:  - Results filtering by Name  - Reset button for filtering

--- src/main/java/my/vaadin/app/MyUI.java
+++ src/main/java/my/vaadin/app/MyUI.java
@@ -4,9 +4,12 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 
 import java.util.List;
 
@@ -22,20 +25,34 @@ public class MyUI extends UI {
 
     private CustomerService service = CustomerService.getInstance();
     private Grid<Customer> grid = new Grid<>(Customer.class);
+    private TextField filterText = new TextField();
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
 
         layout.addComponents(grid);
-
         updateList();
+
+        filterText.setPlaceholder("filter by name...");
+        filterText.addValueChangeListener(e -> updateList());
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button clearFilterTextBtn = new Button(VaadinIcons.CLOSE);
+        clearFilterTextBtn.setDescription("Clear the current filter");
+        clearFilterTextBtn.addClickListener(e -> filterText.clear());
+
+        CssLayout filtering = new CssLayout();
+        filtering.addComponents(filterText, clearFilterTextBtn);
+        filtering.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+
+        layout.addComponents(filtering, grid);
 
         setContent(layout);
     }
 
     public void updateList() {
-        List<Customer> customers = service.findAll();
+        List<Customer> customers = service.findAll(filterText.getValue());
         grid.setItems(customers);
     }
 
